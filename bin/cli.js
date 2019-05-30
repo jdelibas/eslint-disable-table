7ea610cf4d20f6f966f92a61da1c7368d816f380
Josh Cole <saikojosh@users.noreply.github.com>
2019-05-30 14:48:27 +0100

    refactor: move cli code into self-executing async func

--- bin/cli.js
+++ bin/cli.js
@@ -7,30 +7,34 @@ const getTable = require('../src')
 const generateTable = require('../src/generateTable')
 const { commaSeparatedList } = require('../src/optionHandlers');
 
-program
-  .option('-d, --directory <dir>', 'directory to inspect', commaSeparatedList, process.cwd())
-  .option('-j, --json', 'output json', false)
-  .option('-e, --exclude <exclude>', 'exclude patterns, comma separated list', commaSeparatedList)
+(async function main () {
 
-program.on('--help', () => {
-  console.log('')
-  console.log('Examples:')
-  console.log('')
-  console.log('  $ eslint-disable-table -j > table.json')
-  console.log('  $ eslint-disable-table -e .nycoutput, coverage')
-  console.log('  $ eslint-disable-table -d ~/all-my-projects')
-})
+  program
+    .option('-d, --directory <dir>', 'directory to inspect', commaSeparatedList, process.cwd())
+    .option('-j, --json', 'output json', false)
+    .option('-e, --exclude <exclude>', 'exclude patterns, comma separated list', commaSeparatedList)
 
-program.parse(process.argv)
-
-
-
-getTable(program.directory, program.exclude)
-  .then(res => {
-    const output = (program.json ? JSON.stringify(res, null, 4) : generateTable(res, program.directory));
-    console.log(output);
+  program.on('--help', () => {
+    console.log('')
+    console.log('Examples:')
+    console.log('')
+    console.log('  $ eslint-disable-table -j > table.json')
+    console.log('  $ eslint-disable-table -e .nycoutput, coverage')
+    console.log('  $ eslint-disable-table -d ~/all-my-projects')
   })
-  .catch(e => {
-    console.log(e)
-    process.exit(1)
-  })
+
+  program.parse(process.argv)
+
+
+
+  getTable(program.directory, program.exclude)
+    .then(res => {
+      const output = (program.json ? JSON.stringify(res, null, 4) : generateTable(res, program.directory));
+      console.log(output);
+    })
+    .catch(e => {
+      console.log(e)
+      process.exit(1)
+    })
+
+}());
